335fffa098799ed7a14fda4e893dc5422f688061
Irfan Habib <irfan.habib@hpe.com>
2017-02-24 13:15:13 +0000

    API keys template

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 var jsonfile = require('jsonfile');
+var fs = require('fs');
 var config = require('nconf').argv()
     .env()
     .file({file: 'config.json'});
 
+
+if (!fs.existsSync('./api-keys.json')){
+    console.log('No api-keys.json found, please see use api-keys.json.sample as a template');
+    process.exit(1);
+}
 // Load Youtube API Key
 config.defaults(jsonfile.readFileSync('./api-keys.json'));
 
